25c0f11bbea656167c3b043c03f68e95607c1252
* <*>
2025-03-22 16:26:00 +0900

    Init rust project

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    println!("{}", even_or_odd(2));
+}
+
+fn even_or_odd(n: i32) -> &'static str {
+    if n % 2 == 0 {
+        "Even"
+    } else {
+        "Odd"
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_even_or_odd() {
+        assert_eq!(even_or_odd(2), "Even");
+        assert_eq!(even_or_odd(3), "Odd");
+    }
+}
